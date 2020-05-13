34a892ce0b45c7bf82099c6c63a21ddefe534f81
seonk73 <seonk73@naver.com>
2020-05-13 09:23:14 +0900

    admin list_display; models was_published_recently 속성들

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
-
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date infomation', {'fields': ['pub_date'], 'classes': ['collapse']}),
@@ -18,3 +18,5 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
+
+
